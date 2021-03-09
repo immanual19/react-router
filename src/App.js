05120307bbbf2f75160fddbc0c286fe674abae93
immanual19 <immanuelsarker1@gmail.com>
2021-03-09 21:25:46 +0600

    errorFixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router';
 
 const Post = (props) => {
-    const {userId, id, title, body}=props.post;
+    const { id, title, body}=props.post;
     const history=useHistory();
     const showComments=(id)=>{
         const url=`/post/${id}`;
